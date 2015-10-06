8e89ecaf3981b9dabffaed3f5aac8d11ac53c3e5
NULL-OPERATOR <x@cryptolab.net>
2015-10-06 15:24:47 +0100

    potato updates

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,44 @@
 $(document).on('click', '#go-search', function() {
   findImagesOnGoogle({keywords: $('#search-term').val(), container: '#search-results'})
 });
+
+$(document).on('click', '#search-results img', function() {
+  var url = $(this).data('url');
+  $("#workspace img").remove();
+  var img = $("<img>").attr('src', url);
+  $("#workspace").append(img);
+});
+
+$("#search-term").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#go-search").click();
+    }
+});
+
+$(document).on('input', '#text', function() {
+  $("#caption").text($(this).val());
+});
+
+$(document).on('change', '#left', function() {
+  $("#caption").css("left", $(this).val() + 'px');
+});
+
+$(document).on('change', '#top', function() {
+  $("#caption").css("top", $(this).val() + 'px');
+});
+
+$(document).on('change', '#top', function() {
+  $("#caption").css("width", $(this).val() + 'px');
+});
+
+$(document).on('change', '#size', function() {
+  $("#caption").css("width", $(this).val() + 'px');
+});
+
+$(document).on('change', '#colour', function() {
+  $("#caption").css("color", $(this).val());
+});
+
+$(document).on('change', '#align', function() {
+  $("#caption").css("text-align", $(this).val());
+});
